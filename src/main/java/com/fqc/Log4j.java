e3b47bb9267a5a08363d1dd1a8be03261ea5a7c9
Tony Feng <337940626@qq.com>
2019-01-14 22:15:32 +0800

    refactor log4j code

---
+++ src/main/java/com/fqc/Log4j.java
@@ -0,0 +1,17 @@
+package com.fqc;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author TonyFeng
+ */
+public class Log4j {
+    private static final Logger LOGGER = Logger.getLogger(Log4j.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("debug");
+        LOGGER.info("info");
+        LOGGER.warn("warn");
+        LOGGER.error("error");
+    }
+}
--- src/test/java/com/fqc/Log4jTest.java
+++ src/test/java/com/fqc/Log4jTest.java
@@ -5,9 +5,9 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple Log4j.
  */
-public class AppTest 
+public class Log4jTest
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public Log4jTest(String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( Log4jTest.class );
     }
 
     /**
